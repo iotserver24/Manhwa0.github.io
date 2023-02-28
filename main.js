49b9824d7ba8766cfb1ab1898584a905e7d86a81
sandrosos <42159109+sandrosos@users.noreply.github.com>
2023-02-27 23:52:34 -0300

    Add files via upload
    First stage of the site, a lot of things to change and to add.

---
+++ main.js
@@ -0,0 +1,65 @@
+function apareca(int) {
+    if (int==1) {
+        document.getElementById('manhwa1-descricao-hide').className="manhwa1-descricao-show";
+    }
+    if (int==2) {
+        document.getElementById('manhwa1-descricao-hide2').className="manhwa1-descricao-show";
+    }
+    if (int==3) {
+        document.getElementById('manhwa1-descricao-hide3').className="manhwa1-descricao-show";
+    }
+    if (int==4) {
+        document.getElementById('manhwa1-descricao-hide4').className="manhwa1-descricao-show";
+    }
+    if (int==5) {
+        document.getElementById('manhwa1-descricao-hide5').className="manhwa1-descricao-show";
+    }
+    if (int==6) {
+        document.getElementById('manhwa1-descricao-hide6').className="manhwa1-descricao-show";   
+    }
+    if (int==7) {
+        document.getElementById('manhwa1-descricao-hide7').className="manhwa1-descricao-show";
+    }
+    if (int==8) {
+        document.getElementById('manhwa1-descricao-hide8').className="manhwa1-descricao-show";
+    }
+    if (int==9) {
+        document.getElementById('manhwa1-descricao-hide9').className="manhwa1-descricao-show";
+    }
+    if (int==10) {
+        document.getElementById('manhwa1-descricao-hide10').className="manhwa1-descricao-show";
+    }
+}
+
+function suma(int) {
+    if (int==1) {
+        document.getElementById('manhwa1-descricao-hide').className="manhwa1-descricao-hide";
+    }
+    if (int==2) {
+        document.getElementById('manhwa1-descricao-hide2').className="manhwa1-descricao-hide";
+    }
+    if (int==3) {
+        document.getElementById('manhwa1-descricao-hide3').className="manhwa1-descricao-hide";
+    }
+    if (int==4) {
+        document.getElementById('manhwa1-descricao-hide4').className="manhwa1-descricao-hide";
+    }
+    if (int==5) {
+        document.getElementById('manhwa1-descricao-hide5').className="manhwa1-descricao-hide";
+    }
+    if (int==6) {
+        document.getElementById('manhwa1-descricao-hide6').className="manhwa1-descricao-hide";
+    }
+    if (int==7) {
+        document.getElementById('manhwa1-descricao-hide7').className="manhwa1-descricao-hide";
+    }
+    if (int==8) {
+        document.getElementById('manhwa1-descricao-hide8').className="manhwa1-descricao-hide";
+    }
+    if (int==9) {
+        document.getElementById('manhwa1-descricao-hide9').className="manhwa1-descricao-hide";
+    }
+    if (int==10) {
+        document.getElementById('manhwa1-descricao-hide10').className="manhwa1-descricao-hide";
+    }
+}
